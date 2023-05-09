4dc01f4185a13b77680f3b020933c173739e1bb6
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2023-05-08 21:12:12 -0600

    responsive navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -10,76 +10,13 @@ function Navbar() {
   };
 
   return (
-    // <nav ref={navRef} className="navbar">
-    //   <div className="logo">
-    //     <Link to="/#" style={{ textDecoration: "none", color: "black" }}>
-    //       SALMA
-    //     </Link>
-    //   </div>
-
-    //   <div className="nav-links">
-    //     <ul>
-    //       <li className="nav-item">
-    //         <Link
-    //           classname="active"
-    //           to="#about"
-    //           smooth
-    //           style={{ textDecoration: "none", color: "black" }}
-    //         >
-    //           ABOUT
-    //         </Link>
-    //       </li>
-
-    //       <li className="nav-item">
-    //         <Link
-    //           to="#projects"
-    //           smooth
-    //           style={{ textDecoration: "none", color: "black" }}
-    //         >
-    //           PROJECTS
-    //         </Link>
-    //       </li>
-
-    //       <li className="nav-item">
-    //         <Link
-    //           to="#company"
-    //           smooth
-    //           style={{ textDecoration: "none", color: "black" }}
-    //         >
-    //           WHY YOU
-    //         </Link>
-    //       </li>
-
-    //       <li className="nav-item">
-    //         <Link to="/" style={{ textDecoration: "none", color: "black" }}>
-    //           WHY ME
-    //         </Link>
-    //       </li>
-    //       <li className="nav-item">
-    //         <Link to="/" style={{ textDecoration: "none", color: "black" }}>
-    //           CONTACT
-    //         </Link>
-    //       </li>
-    //     </ul>
-    //   </div>
-
-    //   <div className="buttons">
-    //     <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-    //       <FaTimes />
-    //     </button>
-    //     <button className="nav-btn" onClick={showNavbar}>
-    //       <FaBars />
-    //     </button>
-    //   </div>
-    // </nav>
-
     <header>
       <h3>
       <Link to="/#" style={{ textDecoration: "none", color: "black" }}>
         SALMA
        </Link>
       </h3>
-      
+
       <nav ref={navRef}>
         <Link classname="active" to="#about" smooth style={{ textDecoration: "none", color: "black" }}>
           ABOUT
