03dbcb117db8634a926d804648de90caa9035419
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2023-05-08 20:38:16 -0600

    fixed nav bar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,74 +1,110 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./index.scss";
 import { HashLink as Link } from "react-router-hash-link";
-import { faTimes, faBars } from '@fortawesome/free-solid-svg-icons';
-
+import { FaBars, FaTimes } from "react-icons/fa";
 
 function Navbar() {
-
-  const [clicked, setClicked] = useState(false)
-
-  const click = () => {
-    setClicked(current => !current) //changes to whatever it isnt at the time
-  }
-
-  useEffect( () => {
-    console.log(clicked);
-}, [clicked]);
-
+  const navRef = useRef();
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav"); //when function is called we will add or remove this from the className
+  };
 
   return (
-    <nav className="nav">
-      <div className="logo">
-        <Link to="/#about" style={{ textDecoration: "none", color: "black" }}>
-          SALMA
+    // <nav ref={navRef} className="navbar">
+    //   <div className="logo">
+    //     <Link to="/#" style={{ textDecoration: "none", color: "black" }}>
+    //       SALMA
+    //     </Link>
+    //   </div>
+
+    //   <div className="nav-links">
+    //     <ul>
+    //       <li className="nav-item">
+    //         <Link
+    //           classname="active"
+    //           to="#about"
+    //           smooth
+    //           style={{ textDecoration: "none", color: "black" }}
+    //         >
+    //           ABOUT
+    //         </Link>
+    //       </li>
+
+    //       <li className="nav-item">
+    //         <Link
+    //           to="#projects"
+    //           smooth
+    //           style={{ textDecoration: "none", color: "black" }}
+    //         >
+    //           PROJECTS
+    //         </Link>
+    //       </li>
+
+    //       <li className="nav-item">
+    //         <Link
+    //           to="#company"
+    //           smooth
+    //           style={{ textDecoration: "none", color: "black" }}
+    //         >
+    //           WHY YOU
+    //         </Link>
+    //       </li>
+
+    //       <li className="nav-item">
+    //         <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+    //           WHY ME
+    //         </Link>
+    //       </li>
+    //       <li className="nav-item">
+    //         <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+    //           CONTACT
+    //         </Link>
+    //       </li>
+    //     </ul>
+    //   </div>
+
+    //   <div className="buttons">
+    //     <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+    //       <FaTimes />
+    //     </button>
+    //     <button className="nav-btn" onClick={showNavbar}>
+    //       <FaBars />
+    //     </button>
+    //   </div>
+    // </nav>
+
+    <header>
+      <h3>
+      <Link to="/#" style={{ textDecoration: "none", color: "black" }}>
+        SALMA
+       </Link>
+      </h3>
+      
+      <nav ref={navRef}>
+        <Link classname="active" to="#about" smooth style={{ textDecoration: "none", color: "black" }}>
+          ABOUT
         </Link>
-      </div>
+        <Link to="/#projects" smooth style={{ textDecoration: "none", color: "black" }}>
+          PROJECTS
+        </Link>
+        <Link to="/#company" smooth style={{ textDecoration: "none", color: "black" }}> 
+        WHY YOU
+        </Link>
+        <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+          WHY ME
+        </Link>
+        <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+          CONTACT
+        </Link>
 
-      <div className="nav-links">
-      <ul className={clicked ? "navbar active" : "navbar"}>
-          <li className="nav-item">
-            <Link classname="active" to="#about" smooth style={{ textDecoration: "none", color: "black" }}>
-              ABOUT
-            </Link>
-          </li>
-
-          <li className="nav-item">
-            <Link to="#projects" smooth style={{ textDecoration: "none", color: "black" }}>
-              PROJECTS
-            </Link>
-          </li>
-
-          <li className="nav-item">
-            <Link to="#company" smooth style={{ textDecoration: "none", color: "black" }}>
-              WHY YOU
-            </Link>
-          </li>
-
-          <li className="nav-item">
-            <Link to="/" style={{ textDecoration: "none", color: "black" }}>
-              WHY ME
-            </Link>
-          </li>
-
-          <li className="nav-item">
-            <Link to="/" style={{ textDecoration: "none", color: "black" }}>
-              CONTACT
-            </Link>
-          </li>
-        </ul>
-      </div>
-
-      {/* <div id="mobile">
-          <i id="bar"
-          onClick={click} 
-          className={clicked ? faTimes : faBars}
-          ></i>
-        </div> */}
-
-
-
-    </nav>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
   );
 }
 
